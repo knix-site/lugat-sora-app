962ab8e4b23ef49957532863e5b29ec60eb86fc2
knix-site <knixsite@gmail.com>
2026-05-24 16:34:05 +0500

    Update service worker assets with correct app js path

---
+++ www/sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'lugat-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
+
+// Fayllarni telefonga saqlab olish
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Internet bo'lmaganda xotiradan ochish
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
